9dea6e9856fcffdc1be31b1623bbde9145c8e6bc
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-28 14:33:07 +0530

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -97,3 +97,27 @@ resource "aws_ecs_service" "app" {
     container_port   = 80
   }
 }
+
+resource "aws_security_group" "ecs" {
+
+  name   = "${var.environment}-ecs-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
